a5d3912c083a6dd24ee2ac592b5c6ebef8aad848
hkuroki <hkuroki@icloud.com>
2019-09-18 22:55:27 +0900

    Initial Commit

---
+++ Anime2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anime2
+//
+//  Created by 黒木博 on 2019/09/18.
+//  Copyright © 2019 Hiroshi Kuroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
